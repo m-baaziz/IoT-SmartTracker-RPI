ab16110e56901dc0922a4f96d5bdc657bab30763
m-baaziz <mha.baaziz@gmail.com>
2016-11-11 20:37:26 +0100

    gpio test1

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log("Hello World");
-console.log("Ohayoo Gosaymasuu")
+import { gpio } from "pi-gpio"
+
+gpio.open(16, "output", function(err) {		// Open pin 16 for output 
+    gpio.write(16, 1, function() {			// Set pin 16 high (1) 
+        gpio.close(16);						// Close pin 16 
+    });
+});
+
+gpio.open(16, "output", err => {
+	gpio.write(16, 1, () => {
+		gpio.close(16);
+	})
+});
