438177a070ef4209dfa5c012701360801b35322d
m-baaziz <m.baaziz@hotmail.fr>
2016-11-11 21:15:48 +0000

    build serialport

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import gpio from "pi-gpio"
 import SerialPort from "serialport"
 
+console.log("before port instanciation");
+
 const port = new SerialPort("/dev/ttyAMA0", { baudrate: 9600 });
+
+console.log("after port instanciation");
 
 port.on('open', () => {
 	console.log("Port opened")
