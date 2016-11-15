cbec719adb21cbb02579b0a67f48d3029a8154ac
m-baaziz <mha.baaziz@gmail.com>
2016-11-15 16:22:48 +0100

    test 2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import gpio from "pi-gpio"
 import SerialPort from "serialport"
 
-const port = new SerialPort("/dev/ttyAMA0", { baudrate: 9600 });
+const port = new SerialPort("/dev/ttyAMA0", { baudrate: 9600, autoOpen: false });
 
 port.on('open', () => {
 	console.log("Port opened")
+	port.write('AT');
+	port.write(new Buffer('AT'));
 });
 
 port.on('data', (data) => {
@@ -14,3 +16,5 @@ port.on('data', (data) => {
 port.on('error', (err) => {
   console.log('Error: ', err.message);
 })
+
+port.open();
