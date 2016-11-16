d811eaa765d708659d30f670c43bf2e00e7a85ee
m-baaziz <mha.baaziz@gmail.com>
2016-11-16 20:46:27 +0100

    restore last version

--- app.js
+++ app.js
@@ -7,28 +7,27 @@ console.log("after port instanciation");
 
 port.on('open', () => {
 	console.log("Port opened")
-	for (let i = 0; i < 10; i++) {
-		setTimeout(() => {
-			console.log("sending AT : " + i);
-			port.write('AT', () => {
-				port.drain();
-			});
-		}, 2000 * (i+1));
-	}
-	// port.write('AT', () => {
-	// 	port.drain();
-	// });
-	//port.write(new Buffer('AT'));
+	// for (let i = 0; i < 10; i++) {
+	// 	setTimeout(() => {
+	// 		console.log("sending AT : " + i);
+	// 		port.write('AT', () => {
+	// 			port.drain();
+	// 		});
+	// 	}, 2000 * (i+1));
+	// }
+	port.write('AT', () => {
+		port.drain();
+	});
 });
 
-// port.on('data', (data) => {
-//   console.log('Data: ' + data);
-//   if (data == 'P') {
-//     port.write('A', () => {
-// 			port.drain();
-// 		});
-//   }
-// });
+port.on('data', (data) => {
+  console.log('Data: ' + data);
+  if (data == 'P') {
+    port.write('A', () => {
+			port.drain();
+		});
+  }
+});
 
 port.on('error', (err) => {
   console.log('Error: ', err.message);
