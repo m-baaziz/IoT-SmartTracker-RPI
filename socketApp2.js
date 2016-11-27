e430335b5877f8faa88227f86f6fe34e951e3efd
m-baaziz <mha.baaziz@gmail.com>
2016-11-27 18:24:30 +0100

    wifi scanner module

--- socketApp2.js
+++ socketApp2.js
@@ -1,4 +1,5 @@
 import dgram from 'dgram'
+import wifiscanner from 'node-wifiscanner'
 
 const client = dgram.createSocket('udp4');
 
@@ -13,13 +14,18 @@ client.on('message', (msg, rinfo) => {
 
 client.on('listening', () => {
 	client.setBroadcast(true);
+
+	wifiscanner.scan((error, data) => {
+		console.log(`error : ${error}`);
+		console.log('data : ',data);
+	})
 	
-	setInterval(() => {
-		const message = new Buffer("PING");
-		client.send(message, 0, 4, 950, "10.0.0.255", () => {
-			console.log("ping sent");
-		});
-	}, 2000)
+	// setInterval(() => {
+	// 	const message = new Buffer("PING");
+	// 	client.send(message, 0, 4, 950, "10.0.0.255", () => {
+	// 		console.log("ping sent");
+	// 	});
+	// }, 2000)
 })
 
 client.bind(950);
