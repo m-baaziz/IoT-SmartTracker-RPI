87d0a04156a12d6e063b90f5e57ec9e72ba6f63a
m-baaziz <mha.baaziz@gmail.com>
2017-01-08 12:53:57 +0100

    accelerator event emitter

--- vibr.js
+++ vibr.js
@@ -1,13 +1,22 @@
+import EventEmitter from 'events'
 import gpio from 'pi-gpio'
 
 console.log("start")
+
+class Accelerator extends EventEmitter {}
+
+const accelerator = new Accelerator();
+
+accelerator.on('vibration', () => {
+	console.log("VIBRATION");
+})
 
 gpio.open(16, "input", (error) => {
 	if (error) console.log(error)
   setInterval(() => {
 		gpio.read(16, (err, value) => {
 	    if(err) console.log(err);
-	    console.log("val : "+value);
+	    accelerator.emit('vibration')
 		});
-	}, 200);
+	}, 50);
 });
